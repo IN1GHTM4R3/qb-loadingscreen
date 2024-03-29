74303048e56f8b2a8bc8d3ba8ca6ef7e22e03b90
Marvin Brereton <56225640+Developing-Developer2019@users.noreply.github.com>
2024-03-29 20:03:57 +0000

    Updated View
    - Hidden changelogs (these would be better off in the Server Discord)
    - Changed Discord URL to link on Discord Logo
    - Removed script from view and added to app.js

---
+++ html/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.1; // Set volume to 10% (optional)
+    audio.play();
+});
